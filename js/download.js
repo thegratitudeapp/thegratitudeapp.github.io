4e0e68995b1ee270d9c88d3fcbf13390e7001f8b
Pritesh Sankhe <86020614+thegratitudeapp@users.noreply.github.com>
2021-12-17 18:47:08 +0530

    added download.js

---
+++ js/download.js
@@ -0,0 +1,13 @@
+document.getElementById("download_now").addEventListener("click", gtag_report_conversion("https://gratitude.onelink.me/sQxx/app"));
+function gtag_report_conversion(url) {
+  var callback = function () {
+    if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
+  };
+  gtag('event', 'conversion', {
+      'send_to': 'AW-720171978/c1e_CIL6-YoDEMrns9cC',
+      'event_callback': callback
+  });
+  return false;
+}
